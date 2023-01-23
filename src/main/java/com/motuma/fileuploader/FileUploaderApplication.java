1eb32c55cf33b5f07bcebe2f46770accfc9e79d4
motumag <gishumotuma@gmail.com>
2023-01-23 12:42:40 +0300

    file uploader to db and file server

---
+++ src/main/java/com/motuma/fileuploader/FileUploaderApplication.java
@@ -0,0 +1,23 @@
+package com.motuma.fileuploader;
+
+import com.motuma.fileuploader.service.FilesStorageService;
+import jakarta.annotation.Resource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FileUploaderApplication implements CommandLineRunner  {
+    @Resource
+    FilesStorageService storageService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploaderApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/com/motuma/fileuploader/FileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motuma.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
